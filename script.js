a634b0ff24d54e4932d5a7b0b11b1cf75b17e9d8
Minn <radic7700@gmail.com>
2023-11-28 18:10:55 +0900

    초안 완성

---
+++ script.js
@@ -0,0 +1,212 @@
+const input = document.querySelector('#username');
+const userProfile = document.querySelector('.user_profile');
+const latestRepos = document.querySelector('#latest_repos');
+
+// username 입력했을 때 
+input.addEventListener('keydown', getUser);
+
+// 이벤트 
+async function getUser(event) {
+    if (event.keyCode === 13 || event.key === 'Enter') {
+        const username = this.value;
+        
+        const res = await fetch(`https://api.github.com/users/${username}`);
+        console.log(res);
+        if(res.ok) {
+            const data = await res.json();
+            const user = new User(data);
+
+            console.log("user", user);
+
+            // user_profile 노드 추가
+            const {avatarElement, userInfoElement} = createUserNodes(user);
+            userProfile.appendChild(avatarElement);
+            userProfile.appendChild(userInfoElement);
+
+            // repo list 노드 추가
+            const repoList = await getRepoList(username);
+            repoList.forEach((repo) => {
+                const repoItem = createRepoNodes(repo);
+                latestRepos.appendChild(repoItem);
+            });
+
+            console.log("latest Repos", latestRepos);
+        } 
+        else {
+            userProfile.innerHTML = `<div class="error">존재하지 않는 사용자입니다.</div>`;
+            repoTitle.innerHTML = ``;
+        }
+    }
+};
+
+class User {    
+    constructor(data) {
+        let userPropertyList = ["id", "avatar_url", "followers", "following", "public_gists", "public_repos", "company", "location", "email", "blog"];
+        userPropertyList.forEach((property) => {
+            this[property] = data[property];
+        });
+    }
+}
+
+class Repo {
+    constructor(data) {
+        const userId = null;
+        let repoPropertyList = ["id", "name", "html_url", "stargazers_count", "watchers_count", "forks"];
+        repoPropertyList.forEach((property) => {
+            this[property] = data[property];
+        });
+    }
+    
+}
+
+// repo list 만드는 함수
+async function getRepoList(username) {
+    let repoList = [];
+        const res = await fetch(`https://api.github.com/users/${username}/repos`);
+
+        if (res.ok) {
+            const repoTitle = document.querySelector('.repo_title');
+            repoTitle.innerHTML = `Latest Repos`;
+            let dataList = await res.json();
+
+            dataList.forEach((data) => {
+                let repo = new Repo(data);
+                repo.userId = data.owner.id;
+
+                repoList.push(repo);
+            });
+        } else { 
+            console.log("error");
+        }
+        return repoList;
+}
+
+// 노드 생성 및 삽입
+function createUserNodes(user) {
+    const avatarElement = document.createElement('div');
+    avatarElement.classList.add('avatar');
+
+    const avatarImageElement = document.createElement('img');
+    avatarImageElement.setAttribute('src', user.avatar_url);
+
+    const buttonElement = document.createElement('button');
+    buttonElement.innerText = 'view profile';
+    buttonElement.addEventListener('click', () => {
+        window.location.href = user.avatar_url;
+    });
+
+    avatarElement.appendChild(avatarImageElement);
+    avatarElement.appendChild(buttonElement);
+    
+    //user_info 노드
+    const userInfoElement = document.createElement('div');
+    userInfoElement.classList.add('user_info');
+
+    //numbers 노드
+    const numbersElement = document.createElement('div');
+    numbersElement.classList.add('numbers');
+
+    const followersElement = document.createElement('span');
+    followersElement.classList.add('followers');
+    followersElement.innerHTML = `Followers : `
+    followersElement.innerHTML += `<span>${user.followers}</span>`;
+
+    const followingElement = document.createElement('span');
+    followingElement.classList.add('following');
+    followingElement.innerHTML = `Following : `
+    followingElement.innerHTML += `<span>${user.following}</span>`;
+
+    const gistsElement = document.createElement('span');
+    gistsElement.classList.add('gists');
+    gistsElement.innerHTML = `Gists : `
+    gistsElement.innerHTML += `<span>${user.public_gists}</span>`;
+
+    const reposElement = document.createElement('span');
+    reposElement.classList.add('repos');
+    reposElement.innerHTML = `Repos : `
+    reposElement.innerHTML += `<span>${user.public_repos}</span>`;
+
+
+    // about 노드
+    const aboutElement = document.createElement('div');
+    aboutElement.classList.add('about');
+
+    const companyElement = document.createElement('div');
+    companyElement.classList.add('company');
+    companyElement.innerHTML = `Company : `;
+    companyElement.innerHTML += `<span>${user.company}</span>`;
+
+    const locationElement = document.createElement('div');
+    locationElement.classList.add('location');
+    locationElement.innerHTML = `Location : `;
+    locationElement.innerHTML += `<span>${user.location}</span>`;
+
+    const emailElement = document.createElement('div');
+    emailElement.classList.add('email');
+    emailElement.innerHTML = `Email : `;
+    emailElement.innerHTML += `<span>${user.email}</span>`;
+
+    const blogElement = document.createElement('div');
+    blogElement.classList.add('blog');
+    blogElement.innerHTML = `Blog : `;
+    blogElement.innerHTML += `<span>${user.blog}</span>`;
+
+    userInfoElement.appendChild(numbersElement);
+    numbersElement.appendChild(followersElement);
+    numbersElement.appendChild(followingElement);
+    numbersElement.appendChild(gistsElement);
+    numbersElement.appendChild(reposElement);
+
+    userInfoElement.appendChild(aboutElement);
+    aboutElement.appendChild(companyElement);
+    aboutElement.appendChild(locationElement);
+    aboutElement.appendChild(emailElement);
+    aboutElement.appendChild(blogElement);
+
+    return {avatarElement, userInfoElement};
+}
+
+function createRepoNodes(repo) {
+    try {
+        //repo item
+        const repoItemElement = document.createElement('div');
+        repoItemElement.classList.add('repo_item');
+
+        const repoNameElement = document.createElement('span');
+        repoNameElement.classList.add('repo_name');
+        repoNameElement.innerHTML = `<a href="${repo.html_url}">${repo.name}</a>`;
+
+        const repoInfo = document.createElement('div');
+        repoInfo.classList.add('repo_info');
+
+        const starsElement = document.createElement('span');
+        starsElement.classList.add('stars');
+        starsElement.classList.add('repo_info_item');
+        starsElement.innerHTML = `Stars : `;
+        starsElement.innerHTML += `<span>${repo.stargazers_count}</span>`;
+
+        const watchersElement = document.createElement('span');
+        watchersElement.classList.add('watchers');
+        watchersElement.classList.add('repo_info_item');
+        watchersElement.innerHTML = `watchers : `;
+        watchersElement.innerHTML += `<span>${repo.watchers_count}</span>`;
+
+
+        const forksElement = document.createElement('span');
+        forksElement.classList.add('forks');
+        forksElement.classList.add('repo_info_item');
+        forksElement.innerHTML = `forks : `;
+        forksElement.innerHTML += `<span>${repo.forks}</span>`;
+
+        repoItemElement.appendChild(repoNameElement);
+        repoItemElement.appendChild(repoInfo);
+
+        repoInfo.appendChild(starsElement);
+        repoInfo.appendChild(watchersElement);
+        repoInfo.appendChild(forksElement);
+
+        return repoItemElement;
+    } catch (err) {
+        console.log(err);
+    }
+}
